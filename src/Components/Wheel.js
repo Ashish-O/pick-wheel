f47b2e32afb223d0b98fdd7ed33887c2f64c1a38
Ashish-O <ashish2711ojha@gmail.com>
2023-01-12 01:26:45 +0530

    Some Logic Added in Wheel.js

--- src/Components/Wheel.js
+++ src/Components/Wheel.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Data from '../Data/Data';
 export default function Wheel() {
+    
+    const[userList, setUserList] = useState([]);
+    const[winner, setWinner] = useState("?");
+    
+    const loadUsers = () =>{
+        setUserList(Data);
+    }
+
+    useEffect(()=>{
+        loadUsers()
+    },[]);
+
+    const getWinner = () => {
+        let x = Math.random;
+        setWinner(userList[x]);
+    }
+
     return (
     <div>
+        <div>
         {
-            Data.map((data, index) => (
-                <li key={index}>{data.Name}</li>
+            userList.map((user, index) => (
+                <li key={index}>{user.Name}</li>
             ))
         }
+        </div>
+        
+        <button onClick={getWinner}>Start</button>
+        <button>Stop</button>
+
+        <h1>{winner}</h1>
+
     </div>
   )
 }
