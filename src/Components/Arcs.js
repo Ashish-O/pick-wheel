a1863336465f7a60ba32d68172dbbfb8b01e92df
Ashish-O <ashish2711ojha@gmail.com>
2023-01-11 23:08:29 +0530

    initial commit

---
+++ src/Components/Arcs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Arcs() {
+  return (
+    <div>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+    </div>
+  )
+}
